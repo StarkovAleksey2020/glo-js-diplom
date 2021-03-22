3f8c92ab9027be2b59858998bd00945b730c670a
starkovaleksey2020 <starkovaleksey2017@gmail.com>
2021-03-22 16:07:03 +0800

    added: start commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+import './index.html';
+
+
+import './css/animate.css';
+import './css/bootstrap.css';
+import './css/font-awesome.css';
+import './css/jquery.fancybox.css';
+import './css/lightgallery.min.css';
+import './css/settings.css';
+
+import './css/style.css';
+*/
